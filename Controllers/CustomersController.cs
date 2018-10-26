6140b4b0eaec9ec5cc1e2ee2a5493c9ee350cdde
Klolensen <31515047+Klolensen@users.noreply.github.com>
2018-10-26 18:34:32 +0200

    Add files via upload

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using E3RestService.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E3RestService.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Customers")]
+    public class CustomersController : Controller
+    {
+        private static List<Customer> _customerList = new List<Customer>()
+        {
+            new Customer(1, "John", "Doe", 2018),
+            new Customer(2, "Jane", "Doe", 2018),
+            new Customer(3, "James", "Smith", 2018)
+        };
+
+        // GET: api/Customers
+        [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+            return _customerList;
+        }
+
+        // GET: api/Customers/5
+        [HttpGet("{id}", Name = "Get")]
+        public Customer Get(int id)
+        {
+            foreach (Customer customer in _customerList)
+            {
+                if (customer.Id == id)
+                {
+                    return customer;
+                }
+            }
+            throw new FileNotFoundException();
+        }
+        
+        // POST: api/Customers
+        [HttpPost]
+        public void Post([FromBody] Customer customer)
+        {
+            _customerList.Add(customer);
+        }
+        
+        // PUT: api/Customers/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Customer customer)
+        {
+            _customerList.RemoveAll(customerDelegate => customerDelegate.Id == id);
+            _customerList.Add(customer);
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _customerList.RemoveAll(customerDelegate => customerDelegate.Id == id);
+        }
+
+        [HttpGet("FindByYear")]
+        public IEnumerable<Customer> GetCustomerByName([FromQuery] string year)
+        {
+            List<Customer> yearCustomersList = new List<Customer>();
+            foreach (Customer customer in _customerList)
+            {
+                if (customer.RegYear == int.Parse(year))
+                {
+                    yearCustomersList.Add(customer);
+                }
+            }
+            return yearCustomersList;
+        }
+    }
+}
---
+++ Model/Customer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace E3RestService.Model
+{
+    [DataContract]
+    public class Customer
+    {
+        private int _id;
+        private string _firstName;
+        private string _lastName;
+        private int _regYear;
+
+        public Customer()
+        {
+            
+        }
+        
+        public Customer(int id, string firstName, string lastName, int regYear)
+        {
+            _id = id;
+            _firstName = firstName;
+            _lastName = lastName;
+            _regYear = regYear;
+        }
+
+        [DataMember]
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        [DataMember]
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        [DataMember]
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        [DataMember]
+        public int RegYear
+        {
+            get { return _regYear; }
+            set { _regYear = value; }
+        }
+    }
+}
